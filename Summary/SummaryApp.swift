17521318214572519c941f79d375593ce76cbc27
Simon Reid <reidsimon@yahoo.ca>
2026-03-01 08:23:41 -0700

    Initial Commit

---
+++ Summary/SummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryApp.swift
+//  Summary
+//
+//  Created by Simon Reid on 2026-03-01.
+//
+
+import SwiftUI
+
+@main
+struct SummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
